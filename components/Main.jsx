c653f48a2eb0ba71b1722705ed8c96ea1f89483d
Declan-Gallagher23 <83022334+Declan-Gallagher23@users.noreply.github.com>
2025-08-24 20:37:55 +0100

    fix bugs

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,17 +1,21 @@
-import Typed from 'react-typed'
+import {TypeWriter} from 'react-typed'
 import React from 'react'
 
 export default function Main() {
     return (
         <main>
-            <h1> 
-                <Typed strings={"Hello my name is Declan Gallagher"}
-                    typeSpeed={40}
-                    backSpeed={50}
-                    loop
-                />
-                    </h1>
-            <h2>I bring ideas to life on the web with code and creativity.</h2>
-        </main>
+  <h1>
+    <Typewriter
+      options={{
+        strings: ["Hello", "my", "name", "is", "Declan", "Gallagher"],
+        autoStart: true,
+        loop: true,
+        delay: 40,
+        deleteSpeed: 50,
+      }}
+    />
+  </h1>
+  <h2>I bring ideas to life on the web with code and creativity.</h2>
+</main>
     )
 }
