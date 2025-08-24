59baf4ac2a42a4c9b2f9e6f0d30b19ce252484b3
Declan-Gallagher23 <83022334+Declan-Gallagher23@users.noreply.github.com>
2025-08-24 20:11:03 +0100

    adding typed effect to main header

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,10 +1,17 @@
-
+import Typed from 'react-typed'
+import React from 'react'
 
 export default function Main() {
     return (
-        <>
-            <h1> Hello my name is Declan Gallagher</h1>
+        <main>
+            <h1> 
+                <Typed strings={"Hello my name is Declan Gallagher"}
+                    typeSpeed={40}
+                    backSpeed={50}
+                    loop
+                />
+                    </h1>
             <h2>I bring ideas to life on the web with code and creativity.</h2>
-        </>
+        </main>
     )
 }
